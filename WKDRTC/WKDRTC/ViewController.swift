f782e18e0abcf7d493a63ec5236761bafd4c28de
Wayne Dahlberg <waynefj40@gmail.com>
2024-03-09 20:02:15 -0700

    Initial commit

---
+++ WKDRTC/WKDRTC/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WKDRTC
+//
+//  Created by Wayne Dahlberg on 3/9/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
